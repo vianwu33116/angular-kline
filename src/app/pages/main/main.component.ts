9f4533075edb5224f824d681254e5ab3615794c2
vianwu33116 <wserena33116@gmail.com>
2025-06-12 07:57:15 +0800

    remove unused import

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BinanceApiService } from 'src/app/services/binance-api.service';
-import { Observable } from 'rxjs';
 import { Currency } from 'src/app/models/currency.model';
 
 @Component({
